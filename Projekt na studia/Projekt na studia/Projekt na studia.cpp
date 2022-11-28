6ec7f0f82f85b095841862dff6901fc70bbd5301
Jakub Szklarz <kubusisko1@gmail.com>
2022-11-28 22:48:10 +0100

    sfml
    instalacja i test

--- Projekt na studia/Projekt na studia/Projekt na studia.cpp
+++ Projekt na studia/Projekt na studia/Projekt na studia.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+int main() {
+
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML Works");
+
+
+
+	while (window.isOpen())
+
+	{
+
+		window.clear(sf::Color::Black);
+
+		sf::RectangleShape rs(sf::Vector2f(100, 100));
+
+		window.draw(rs);
+
+		window.display();
+
+	}
+
 }
